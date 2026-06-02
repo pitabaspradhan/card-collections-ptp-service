76438a15b188a17e64305a8fbfe66eb049c1af5d
Pitabas Pradhan <pitabaspradhan@gmail.com>
2026-06-02 18:52:54 +0530

    Add infrastructure layer and repository implementation.

---
+++ CardCollections.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CardCollections.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CardCollections.Infrastructure/Idempotency/InMemoryCollectionCaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using CardCollections.Application.Interfaces;
+using CardCollections.Domain.Entities;
+using System.Collections.Concurrent;
+
+namespace CardCollections.Infrastructure.Repositories;
+
+public sealed class InMemoryCollectionCaseRepository
+    : ICollectionCaseRepository
+{
+    private readonly ConcurrentDictionary<Guid, CollectionCase> _store = new();
+
+    public Task AddAsync(CollectionCase collectionCase)
+    {
+        _store.TryAdd(collectionCase.Id, collectionCase);
+        return Task.CompletedTask;
+    }
+
+    public Task<CollectionCase?> GetByIdAsync(Guid caseId)
+    {
+        _store.TryGetValue(caseId, out var collectionCase);
+        return Task.FromResult(collectionCase);
+    }
+
+    public Task SaveAsync(CollectionCase collectionCase)
+    {
+        _store[collectionCase.Id] = collectionCase;
+        return Task.CompletedTask;
+    }
+}
