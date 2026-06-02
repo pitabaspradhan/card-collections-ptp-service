70afc2e6cda60de993dafbd3860016254783d19d
Pitabas Pradhan <pitabaspradhan@gmail.com>
2026-06-02 20:47:02 +0530

    Add  dependency injection.

--- src/CardCollections.Api/CardCollections.Api/Program.cs
+++ src/CardCollections.Api/CardCollections.Api/Program.cs
@@ -1,3 +1,9 @@
+using CardCollections.Application.Interfaces;
+using CardCollections.Application.Services;
+using CardCollections.Infrastructure.Events;
+using CardCollections.Infrastructure.Idempotency;
+using CardCollections.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,20 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+builder.Services.AddSingleton<ICollectionCaseRepository,
+    InMemoryCollectionCaseRepository>();
+
+builder.Services.AddSingleton<IIdempotencyStore,
+    InMemoryIdempotencyStore>();
+
+builder.Services.AddSingleton<IEventPublisher,
+    FakeEventPublisher>();
+
+builder.Services.AddScoped<ICollectionCaseService,
+    CollectionCaseService>();
+
+builder.Services.AddScoped<IPromiseToPayService,
+    PromiseToPayService>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
