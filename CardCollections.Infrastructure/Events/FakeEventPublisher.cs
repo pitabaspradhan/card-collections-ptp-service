11b698a6c021c6290af4e9f747068c1d85e8c904
Pitabas Pradhan <pitabaspradhan@gmail.com>
2026-06-02 18:57:49 +0530

    Implement infrastructure components.

---
+++ CardCollections.Infrastructure/Events/FakeEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using CardCollections.Application.Interfaces;
+
+namespace CardCollections.Infrastructure.Events;
+
+public sealed class FakeEventPublisher : IEventPublisher
+{
+    public Task PublishAsync<T>(T @event)
+    {
+        Console.WriteLine(
+            $"Event Published: {typeof(T).Name}");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ CardCollections.Infrastructure/Idempotency/InMemoryIdempotencyStore.cs
@@ -0,0 +1,20 @@
+﻿using CardCollections.Application.Interfaces;
+using System.Collections.Concurrent;
+
+namespace CardCollections.Infrastructure.Idempotency;
+
+public sealed class InMemoryIdempotencyStore : IIdempotencyStore
+{
+    private readonly ConcurrentDictionary<string, bool> _keys = new();
+
+    public Task<bool> ExistsAsync(string key)
+    {
+        return Task.FromResult(_keys.ContainsKey(key));
+    }
+
+    public Task StoreAsync(string key)
+    {
+        _keys.TryAdd(key, true);
+        return Task.CompletedTask;
+    }
+}
